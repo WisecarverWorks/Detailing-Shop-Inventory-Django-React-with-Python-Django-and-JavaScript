4b497d3ca41a705bdbb5fa71a8342fdddcc2541e
Kyle Wisecarver <wiseworks.2022@gmail.com>
2023-02-13 12:53:22 -0600

    try catch for local_settings.py which will give us our SECRET_KEY and keep it local.

--- detail_shop_api/detail_shop_api/settings.py
+++ detail_shop_api/detail_shop_api/settings.py
@@ -8,8 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8i0(a3q%&o88%%05$zsq3q4@v^jeoy$5hi=8*l7s3bi@5+^nbp'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -110,3 +109,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try :
+    from .local_settings import *
+    
+except ImportError:
+    pass
